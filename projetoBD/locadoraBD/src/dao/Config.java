e9daa4dab67b76d77d108019a1e32d97ee6647c6
marcosrrosa <39974270+marcosrrosa@users.noreply.github.com>
2018-06-25 23:36:59 -0300

    Add files via upload

---
+++ projetoBD/locadoraBD/src/dao/Config.java
@@ -0,0 +1,7 @@
+package dao;
+
+public class Config {
+    public static final String URL = "jdbc:mysql://localhost/mysql?useSSL=false";
+    public static final String LOGIN = "root";
+    public static final String PASSWORD = "root";
+}
