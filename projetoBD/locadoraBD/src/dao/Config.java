2d472a6e141b398620f045a2d8bc47082ca1f332
tonhozera <marcosrosa@alunos.utfpr.edu.br>
2018-06-27 19:02:59 -0300

    remoção da pasta projetoBD para reupar a atualização

--- projetoBD/locadoraBD/src/dao/Config.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-public class Config {
-    public static final String URL = "jdbc:mysql://localhost/mysql?useSSL=false";
-    public static final String LOGIN = "root";
-    public static final String PASSWORD = "root";
-}
